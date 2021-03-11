0b7730f8b03e42ccaa7663c406cdec503415499e
dgeri <goroshilovadaria@gmail.com>
2021-03-11 17:47:34 +0300

    add videos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,21 +3,20 @@ import React, { useState, useEffect } from "react";
 export default function Videos({ videos }) {
   return (
     <div className="video-wrapper">
-      <div className="videos">
-        {videos &&
-          videos.map((v) => {
-            var wrongLink = "watch?v=";
-            var rightLink = "embed/";
-            if (videos) {
-              return (
-                <iframe
-                  width="300"
-                  src={v.uri.replace(wrongLink, rightLink)}
-                ></iframe>
-              );
-            }
-          })}
-      </div>
+      {videos &&
+        videos.map((v) => {
+          var wrongLink = "watch?v=";
+          var rightLink = "embed/";
+          if (videos) {
+            return (
+              <iframe
+                src={v.uri.replace(wrongLink, rightLink)}
+              ></iframe>
+            );
+          } else {
+            return <div></div>;
+          }
+        })}
     </div>
   );
 }
